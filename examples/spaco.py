3e90ce3804979a861b45f274b2ebcb0a1773e7d9
Ma Fan <mafan@grmdept.grm.cuhk.edu.hk>
2017-10-10 11:06:38 +0800

    test

---
+++ examples/spaco.py
@@ -0,0 +1,20 @@
+from __future__ import print_function, absolute_import
+import argparse
+import os.path as osp
+
+import numpy as np
+import sys
+import torch
+from torch import nn
+from torch.backends import cudnn
+from torch.utils.data import DataLoader
+
+from reid import datasets
+from reid import models
+from reid.dist_metric import DistanceMetric
+from reid.trainers import Trainer
+from reid.evaluators import Evaluator
+from reid.utils.data import transforms as T
+from reid.utils.data.preprocessor import Preprocessor
+from reid.utils.logging import Logger
+from reid.utils.serialization import load_checkpoint, save_checkpoint
