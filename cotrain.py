e02de9fee60b77ce538259d6e14f2a111f9a6ef0
Flwoerfan <flower.fan@foxmail.com>
2018-01-21 18:33:32 +0800

    cotrain

--- cotrain.py
+++ cotrain.py
@@ -25,12 +25,20 @@ def cotrain(configs,data,iter_step=1,train_ratio=0.2):
     assert iter_step >= 1
     train_data,untrain_data = dp.split_dataset(data.trainval, train_ratio)
     data_dir = data.images_dir
+    
     for step in range(iter_step):
         pred_probs = []
         add_ids = []
         for view in range(2):
             configs[view].set_training(True)
             model = mu.train(train_data, data_dir, configs[view])
+            save_checkpoint({
+                'state_dict': model.state_dict(),
+                'epoch': step +1}, False,
+                fpath = os.path.join(configs[view].logs_dir, configs[view].model_name, 'cotrain.epoch%d' % step)
+            )
+            if len(untrain_data) == 0:
+                continue
             pred_probs.append(mu.predict_prob(
                 model,untrain_data,data_dir,configs[view]))
             add_ids.append(dp.sel_idx(pred_probs[view], train_data))
@@ -42,20 +50,16 @@ def cotrain(configs,data,iter_step=1,train_ratio=0.2):
             print(np.mean(t_y == p_y))
 #             evaluation current model and save it
             # mu.evaluate(model,data,configs[view])
-            save_checkpoint({
-                'state_dict': model.state_dict(),
-                'epoch': step +1}, False,
-                fpath = os.path.join(configs[view].logs_dir, configs[view].model_name, 'cotrain.epoch%d' % step)
-            )
         
             mu.evaluate(model, data, configs[view])
-
+        
+        if len(untrain_data) == 0:
+            break
+            
         pred_y = np.argmax(sum(pred_probs), axis=1)
         add_id = sum(add_ids)
         train_data, untrain_data = dp.update_train_untrain(
             add_id,train_data,untrain_data,pred_y)
-        if len(untrain_data) == 0:
-            break
 
 
 config1 = Config()
