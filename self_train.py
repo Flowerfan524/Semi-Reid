4a05b6bbfbf336dd814364fd220ccd1d4ce5a724
Flowerfan524 <flowerfan524@gmail.com>
2018-02-07 09:31:34 +0800

    fix bugs

--- self_train.py
+++ self_train.py
@@ -44,12 +44,18 @@ def self_train(configs, data, iter_step=1, train_ratio=0.2):
                 fpath=os.path.join(
                     configs[view].logs_dir, configs[view].model_name, 'self_train.epoch%d' % step)
             )
-            if len(untrain_data) == 0:
+            # calculate predict probility on all data
+            p_b = mu.predict_prob(model, data.trainval, data_dir, configs[view])
+            p_y = np.argmax(p_b, axis=1)
+            t_y = [c for (_,c,_,_) in data.trainval]
+            print(np.mean(t_y == p_y))
+
+            if len(new_untrain_data) == 0:
                 break
             pred_prob = mu.predict_prob(
                 model, new_untrain_data, data_dir, configs[view])
             pred_y = np.argmax(pred_prob, axis=1)
-            add_id = dp.sel_idx(pred_prob, train_data, add_ratio)
+            add_id = dp.sel_idx(pred_prob, new_train_data, add_ratio)
             new_train_data, new_untrain_data = dp.update_train_untrain(
                 add_id, new_train_data, new_untrain_data, pred_y)
 
